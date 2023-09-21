01f60bcfd07df4753f099ddb26d5af8ceea4d01b
Vitório Gabriel <126120678+vitoriogabriel@users.noreply.github.com>
2023-09-21 15:09:07 -0300

    Add files via upload

---
+++ medialuno_java/medialuno.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class medialuno {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a primeira nota: ");
+        double n1 = scanner.nextDouble();
+        System.out.print("Digite a segunda nota: ");
+        double n2 = scanner.nextDouble();
+        System.out.print("Digite a terceira nota: ");
+        double n3 = scanner.nextDouble();
+
+        double media = (n1 + n2 + n3) / 3;
+
+        if (media >= 6) {
+            System.out.println("Aprovado! ");
+        } else {
+            System.out.println("Reprovado :( ");
+        }
+        System.out.println("Sua média é: " + media);
+        scanner.close();
+    }
+}
